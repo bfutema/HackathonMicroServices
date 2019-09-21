a0b5a6bb6e782eef43ae17d903e1d05371614d80
Bruno Amaral Futema <bruno.futema@outlook.com>
2019-09-21 20:32:52 -0300

    ajustando erros

---
+++ api-messages/src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const routeMessage = require("./routes/message.js");
+
+const app = express();
+
+app.use(routeMessage);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  next();
+});
+
+app.listen(9001);
